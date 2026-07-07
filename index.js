634f047f9fbdaabc2f59703ec0ce6434d02e3022
Harbey Graphics <harbeygraphics25@gmail.com>
2026-07-07 12:15:37 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+function formatDay(timestamp) {
+	let date = new Date(timestamp * 1000);
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	let day = days[date.getDay()];
+	return day;
+}
+
+function getForecast(city) {
+	let apiKey = "9f1739f0t2608f809957ea4ea4ob5f0b";
+	let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(showForecast);
+}
+
+function showForecast(response) {
+	// console.log(response.data);
+
+	let forecastHtml = "";
+
+	response.data.daily.forEach(function (day, index) {
+		if (index < 5) {
+			forecastHtml =
+				forecastHtml +
+				`<div class="weather-forecast-day">
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+<div ><img class="weather-forecast-icon" src=${day.condition.icon_url}></div>
+<div class="weather-forecast-temperatures">
+	<div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°C</strong></div>
+	<div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°C</div>
+</div>
+</div>`;
+		}
+	});
+
+	let forecastElement = document.querySelector("#forecast");
+	forecastElement.innerHTML = forecastHtml;
+}
+
 function showData(response) {
 	let temperatureElement = document.querySelector("#temperature");
 	let weatherCondition = document.querySelector(".weather-condition");
@@ -33,6 +70,8 @@ function showData(response) {
 	city.innerHTML = response.data.city;
 	timeElement.innerHTML = `${day} ${hour}:${minute}`;
 	iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" />`;
+
+	getForecast(response.data.city);
 }
 
 function searchCity(city) {
